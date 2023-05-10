6882f66286c0f1c13fda8ac3320046bb3c76f476
Antoine Haddad <89608252+YourAntidote@users.noreply.github.com>
2023-05-09 23:33:36 -0400

    Update 2.3a

--- script/script.js
+++ script/script.js
@@ -1,4 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(MorphSVGPlugin);
+gsap.registerPlugin(DrawSVGPlugin);
+
 
 gsap.from(".container", {
   opacity: 0,
@@ -50,21 +54,21 @@ window.addEventListener('scroll', function() {
   clearTimeout(scrollTimer);
   scrollTimer = setTimeout(function() {
     body.classList.remove('is-scrolling');
-  }, 200);
+  }, 100);
 });
 
 
 /* --------------------------------------Animation Chapitre 1 --------------------------------------------*/
 
 
-gsap.timeline({ scrollTrigger: {
-    pin:true,
+gsap.timeline({ 
+  scrollTrigger: {
     markers: true,
     scrub: true,
     start: 'top top',
     end:"500% top" ,
     trigger: '#chapitre1',
-    toggleActions: "play reverse reverse play"
+    toggleActions: "restart complete reverse reset"
   }})
 .to('#homme-falaise', {
   y: '50vh',
@@ -73,16 +77,18 @@ gsap.timeline({ scrollTrigger: {
 .from('#ixy-mort', {
   y: '100vh',
   duration: 1,
+  opacity: 0
 });
 
-gsap.timeline({ scrollTrigger: {
+gsap.timeline({ 
+  scrollTrigger: {
   pin:true,
   markers: true,
   scrub: true,
   start: 'top top',
   end:"500% top" ,
   trigger: '#chapitre1',
-  toggleActions: "play reverse reverse play"
+  toggleActions: "restart complete reverse reset"
 }})
 .to('.petite-roche', {
   y: '100vh',
@@ -101,19 +107,8 @@ let anim = gsap.timeline()
 
 /* --------------------------------------Animation Chapitre 2 --------------------------------------------*/
 
-gsap.timeline({ scrollTrigger: {
-  pin:true,
-  markers: true,
-  scrub: true,
-  start: 'top top',
-  end:"400% top" ,
-  trigger: '#chapitre2',
-  toggleActions: "play reverse reverse play"
-  }
-})
-.from('.sprite-animation', {
-  x: '-100vw',
-});
+
+
 
 gsap.timeline({ scrollTrigger: {
   pin:true,
@@ -122,7 +117,7 @@ gsap.timeline({ scrollTrigger: {
   start: 'top top',
   end:"400% top" ,
   trigger: '#chapitre2',
-  toggleActions: "play reverse reverse play"
+  toggleActions: "restart complete reverse reset"
   }
 })
 .to('.feuille.no0', {
@@ -130,14 +125,39 @@ y: '90vmin',
 duration: 1,
 rotation: 360
 })
-.from('.carte', {
-opacity: 0,
-duration: 1,
-
-
-})
+.from('.sprite-animation', {x: '-90vw',},'<')
+.to('.feuille.no0',{ opacity: 0})
+.to('.sprite-animation',{ opacity: 0,},'<')
+.to('#ixy-feuille',{  opacity: 1},'<')
 
 /* --------------------------------------Animation Chapitre 3 --------------------------------------------*/
+
+let animation;
+
+gsap.timeline({ scrollTrigger: {
+  pin:true,
+  markers: true,
+  scrub: true,
+  start: 'top top',
+  end:"400% top" ,
+  trigger: '#chapitre3',
+  toggleActions: "restart complete reverse reset"
+  }
+})
+.to('.ixy', {
+  motionPath: {
+    align: '#trajet',
+    path: '#trajet',
+    alignOrigin: [0.5, 0.5],
+    xPercent: 50,
+    xPercent: 50,
+    autoRotate: false,
+    start: 0,
+    end: 1
+  },
+
+  repeat: 0,
+})
 
 /* --------------------------------------Animation Chapitre 4 --------------------------------------------*/
 
@@ -146,23 +166,47 @@ gsap.timeline({ scrollTrigger: {
     markers: true,
     scrub: true,
     start: 'top top',
-    end:"500% top" ,
-    trigger: '#chapitre1',
-    toggleActions: "play reverse reverse play"
+    end:"200% top" ,
+    trigger: '#chapitre4',
+    toggleActions: "restart complete reverse reset"
   }})
-.to('#homme-falaise', {
-  y: '50vh',
-  duration: 1,
-})
-.from('#ixy-mort', {
-y: '100vh',
-duration: 1,
-opacity: 0,
-});
+
 
 /* --------------------------------------Animation Chapitre 5 --------------------------------------------*/
+gsap.timeline({ scrollTrigger: {
+  pin:true,
+  markers: true,
+  scrub: true,
+  start: 'top top',
+  end:"400% top" ,
+  trigger: '#chapitre5',
+  toggleActions: "restart complete reverse reset"
+  }
+})
+
+const mp3 = document.getElementById('coffre');
+
+let play = document.querySelector("#chapitre5");
+play.addEventListener("click", function () {
+  mp3.play();
+});
+
+
 
 /* --------------------------------------Animation Chapitre 6 --------------------------------------------*/
+
+gsap.timeline({ scrollTrigger: {
+  pin:true,
+  markers: true,
+  scrub: true,
+  start: 'top top',
+  end:"400% top" ,
+  trigger: '#chapitre6',
+  toggleActions: "restart complete reverse reset"
+  }
+})  
+.from(".path", {ease: "none", drawSVG: "0% 0%"})
+
 
 let animFeuille = gsap.timeline()
   .paused(false)
@@ -173,21 +217,64 @@ let animFeuille = gsap.timeline()
     .to('.feuille.no5', { duration: 15, y: '48vmin', rotation: 360, repeat: '-1', ease: 'none'}, '<')
 
     
-
-
-
-
-/*gsap.to(
-  "#chapitre2",
-  {
-    scrollTrigger: {
-  trigger: "#chapitre2",
-  start: "50% 50%", 
-  pin: true,
-  scrub: true,
-
-  }
-}
-);
-*/
 /* --------------------------------------Animation Chapitre 7 --------------------------------------------*/
+
+gsap.timeline({ scrollTrigger: {
+  pin:true,
+  markers: true,
+  scrub: true,
+  start: 'top top',
+  end:"400% top" ,
+  trigger: '#chapitre7',
+  toggleActions: "restart complete reverse reset"
+  }
+})
+gsap.to("#chapitre7", {
+  backgroundPosition: "0% 90%",
+  ease: "none",
+  scrollTrigger: {
+      trigger: "#chapitre7",
+      start: 'top top',
+      end:"400% top" ,
+      scrub: true,
+      markers: true,
+      toggleActions: "restart complete reverse reset"
+  }
+})
+gsap.to('.nuage.no1', {
+  bottom: '-100vh',
+  ease: "none",
+  scrollTrigger: {
+      trigger: "#chapitre7",
+      start: 'top top',
+      end:"400% top" ,
+      scrub: true,
+      markers: true,
+      toggleActions: "restart complete reverse reset"
+  }
+})
+gsap.to('.nuage.no2', {
+  bottom: '-50vh',
+  ease: "none",
+  scrollTrigger: {
+      trigger: "#chapitre7",
+      start: 'top top',
+      end:"400% top" ,
+      scrub: true,
+      markers: true,
+      toggleActions: "restart complete reverse reset"
+  }
+})
+
+
+gsap.timeline({
+  scrollTrigger: {
+    start: 'top top',
+    end:"400% top" ,
+    scrub: 0.5,
+    trigger: "#chapitre7",
+    toggleActions: "restart complete reverse reset"
+  },
+})
+
+.from("#nuage", {morphSVG: "#fantome"});
